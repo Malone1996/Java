0023c27a11a51bef34392081c602108aadb7acf6
Malone1996 <mayaobo1996@qq.com>
2017-12-08 17:16:02 +0800

    guess number

---
+++ guessnum.java
@@ -0,0 +1,33 @@
+package guess;
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessnum {
+	public static void main(String []arg){
+		Scanner input =new Scanner(System.in);
+		Random random=new Random();
+		int s=random.nextInt(100)+1;
+		System.out.println("请输入你猜的数(1-100):");
+		
+		for (int i = 0; i < 100; i++){
+		
+		int num1=input.nextInt();
+		
+		if (num1>s){
+			System.out.println("你猜的大了！");
+			System.out.println("请再次输入你猜的数(1-100):");
+		}
+		else if (num1<s){
+			System.out.println("你猜的小了！");
+			System.out.println("请再次输入你猜的数(1-100):");
+			
+		}
+		
+		else{
+			System.out.println("你猜对了！");
+			System.out.println("game over！");
+		}
+		}
+	}
+	
+}
